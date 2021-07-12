593f3bd51aa15228861621835c8b4e12f2df4976
Andreribas15 <54609210+Andreribas15@users.noreply.github.com>
2021-07-12 12:15:04 -0300

    Update and rename dev.php to site.php

--- commands/dev.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$Bot->command("/\/dev/", function ($Update, $Match) use ($Bot, $LANG) {
-    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["DEV"]]);
-});
